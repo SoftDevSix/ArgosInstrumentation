1d71f31bab7d9a39bd0cb6974365ed05badda6c8
SebasthianSalpozz <sebasthiansalinaspozzo@gmail.com>
2024-11-20 10:22:37 -0400

    feat: add coverage adapter overwriting the visit method

---
+++ src/main/java/edu/usb/argosinstrumentation/CoverageAdapter/CoverageAdapter.java
@@ -0,0 +1,20 @@
+package edu.usb.argosinstrumentation.CoverageAdapter;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class CoverageAdapter extends ClassVisitor implements Opcodes {
+    String className;
+
+    public CoverageAdapter(ClassVisitor cv, String className) {
+        super(Opcodes.ASM5, cv);
+        this.className = className;
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
+        return new CodeInjecter(mv, name, desc, className);
+    }
+}
