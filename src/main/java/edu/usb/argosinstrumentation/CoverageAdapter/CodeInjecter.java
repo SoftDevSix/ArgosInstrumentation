9634b5089069b68a27855ed3c959d6444984d685
Alex Paca <fujorita772@gmail.com>
2024-11-20 09:39:51 -0400

    feat: code injecter

---
+++ src/main/java/edu/usb/argosinstrumentation/CoverageAdapter/CodeInjecter.java
@@ -0,0 +1,20 @@
+package edu.usb.argosinstrumentation.CoverageAdapter;
+
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class CodeInjecter extends MethodVisitor implements Opcodes{
+    private final String methodName;
+    private final String methodDesc;
+    private final String className;
+
+    public CodeInjecter(MethodVisitor mv, String name, String desc, String className) {
+        super(ASM5, mv);
+        this.methodName = name;
+        this.methodDesc = desc;
+        this.className = className;
+    }
+
+}
+
