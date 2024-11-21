4c4a699a43277758de515bcf1d6942cbe7a0b776
JosuePrado <prado69508698@gmail.com>
2024-11-20 20:07:00 -0400

    feat: implement information collector base class

---
+++ src/main/java/edu/usb/argosinstrumentation/informationCollector/Counter.java
@@ -0,0 +1,8 @@
+package edu.usb.argosinstrumentation.InformationCollector;
+
+public class Counter {
+  
+  public Counter(MethodVisitor mv, FirstPassInfo myInfo, String methodName, String methodDesc) {
+    
+  }
+}
---
+++ src/main/java/edu/usb/argosinstrumentation/informationCollector/InformationCollector.java
@@ -0,0 +1,22 @@
+package edu.usb.argosinstrumentation.InformationCollector;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor; 
+import org.objectweb.asm.Opcodes;
+
+public class InformationCollector extends ClassVisitor implements Opcodes {
+  private final ClassData classData;
+  private final String className;
+
+  public InformationCollector(ClassVisitor classVisitor, ClassData classData, String className) {
+    super(Opcodes.ASM5, classVisitor);
+    this.classData = classData;
+    this.className = className;
+  }
+
+  @Override
+  public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+    MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
+    return new Counter(mv, classData, name, desc);
+  }
+}
