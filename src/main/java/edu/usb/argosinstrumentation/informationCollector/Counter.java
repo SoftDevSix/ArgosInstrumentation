1ef9813356c2892ff005b2dee7ed963b538b229c
jhael-arce-jalafund <jhael.arce.8041022@usalesiana.edu.bo>
2024-11-20 21:24:02 -0400

    feat: Counter class implemented

--- src/main/java/edu/usb/argosinstrumentation/informationCollector/Counter.java
+++ src/main/java/edu/usb/argosinstrumentation/informationCollector/Counter.java
@@ -2,10 +2,17 @@ package edu.usb.argosinstrumentation.informationCollector;
 
 import edu.usb.argosinstrumentation.domain.ClassData;
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
-public class Counter {
-  
-  public Counter(MethodVisitor mv, ClassData myInfo, String methodName, String methodDesc) {
-    
+public class Counter extends MethodVisitor implements Opcodes {
+  private final ClassData classData;
+  private final String methodName;
+  private final String methodDesc;
+
+  public Counter(MethodVisitor mv, ClassData classData, String methodName, String methodDesc) {
+    super(ASM5, mv);
+    this.classData = classData;
+    this.methodName = methodName;
+    this.methodDesc = methodDesc;
   }
 }
