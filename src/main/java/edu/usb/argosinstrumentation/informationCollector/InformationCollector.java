6977471460265311c7b7ee101b1e4019aebf329a
Josue_prado <prado69508698@gmial.com>
2024-11-24 21:07:14 -0400

    feat: implement test for information collector and counter

--- src/main/java/edu/usb/argosinstrumentation/informationCollector/InformationCollector.java
+++ src/main/java/edu/usb/argosinstrumentation/informationCollector/InformationCollector.java
@@ -2,22 +2,22 @@ package edu.usb.argosinstrumentation.informationCollector;
 
 import edu.usb.argosinstrumentation.domain.ClassData;
 import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.MethodVisitor; 
+import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
 public class InformationCollector extends ClassVisitor implements Opcodes {
-  private final ClassData classData;
-  private final String className;
+    private final ClassData classData;
 
-  public InformationCollector(ClassVisitor classVisitor, ClassData classData, String className) {
-    super(Opcodes.ASM5, classVisitor);
-    this.classData = classData;
-    this.className = className;
-  }
+    public InformationCollector(ClassVisitor classVisitor, ClassData classData) {
+        super(Opcodes.ASM5, classVisitor);
+        this.classData = classData;
+    }
 
-  @Override
-  public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-    MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
-    return null;
-  }
+    @Override
+    public MethodVisitor visitMethod(
+            int access, String name, String desc, String signature, String[] exceptions) {
+        MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
+        Counter counter = new Counter(mv, classData, name, desc);
+        return counter;
+    }
 }
