aa7c95adab3b09a498fa791c688769bade91ab58
bianca-soliz-jalafound <rbiancasoliz@gmail.com>
2024-11-27 11:05:37 -0400

    feat: pass one of coverage transformer

--- src/main/java/edu/usb/argosinstrumentation/transformer/CoverageTransformer.java
+++ src/main/java/edu/usb/argosinstrumentation/transformer/CoverageTransformer.java
@@ -1,6 +1,8 @@
 package edu.usb.argosinstrumentation.transformer;
 
+import edu.usb.argosinstrumentation.collector.InformationCollector;
 import edu.usb.argosinstrumentation.coverageadapter.CoverageAdapter;
+import edu.usb.argosinstrumentation.domain.ClassData;
 import lombok.RequiredArgsConstructor;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
@@ -8,6 +10,16 @@ import org.objectweb.asm.ClassWriter;
 @RequiredArgsConstructor
 public class CoverageTransformer {
     private final String projectName;
+
+    byte[] passOne(byte[] classByte, ClassData classData) {
+        byte[] ret;
+        ClassReader classReader = new ClassReader(classByte);
+        ClassWriter classWriter = new ClassWriter(classReader, 0);
+        InformationCollector classVisitor = new InformationCollector(classWriter, classData);
+        classReader.accept(classVisitor, 0);
+        ret = classWriter.toByteArray();
+        return ret;
+    }
 
     byte[] passTwo(byte[] classByte, String name) {
         byte[] ret;
